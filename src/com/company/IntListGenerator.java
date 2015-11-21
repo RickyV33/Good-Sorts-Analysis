efb2b13ee3bec64482e612a04e5cb4bf2860cd32
Ricky Valencia <3d.rickyV@gmail.com>
2015-11-21 15:26:45 -0800

    Add integer list generator

---
+++ src/com/company/IntListGenerator.java
@@ -0,0 +1,63 @@
+package com.company;
+
+import java.util.Random;
+
+public class IntListGenerator {
+
+    public int[] partiallySortedList(int size, int bound, int percent) {
+        Random random = new Random();
+        int[] list = new int[size];
+        double sizeOfSorted = (percent / 100.0) * size;
+
+        for (int i = 0; i < sizeOfSorted; ++i) {
+            list[i] = i;
+        }
+        for (int i = (int)sizeOfSorted; i < size; ++i) {
+           list[i] = random.nextInt(bound);
+        }
+        return list;
+    }
+
+    public int[] sortedList(int size, int bound, boolean ascending) {
+        int[] list = new int[size];
+
+        if (ascending) {
+            for (int i = 0; i < size; ++i) {
+                list[i] = i;
+            }
+        }
+        else {
+            for (int i = size-1; i > -1; --i) {
+                list[i] = i;
+            }
+        }
+
+        return list;
+    }
+
+    public int[] randomList(int size, int bound) {
+        Random random = new Random();
+        int[] list = new int[size];
+
+        for (int i = 0; i < size; ++i) {
+            list[i] = random.nextInt(bound);
+        }
+        return list;
+    }
+
+    public int[] repeatingList(int size, int bound, int sizeOfPattern) {
+        Random random = new Random();
+        int[] list = new int[size];
+        int[] pattern = new int[sizeOfPattern];
+
+        for (int i = 0; i < sizeOfPattern; ++i) {
+            pattern[i] = random.nextInt(bound);
+        }
+
+        for (int i = 0; i < size; ++i) {
+            list[i] = pattern[i % sizeOfPattern];
+        }
+
+        return list;
+    }
+}
